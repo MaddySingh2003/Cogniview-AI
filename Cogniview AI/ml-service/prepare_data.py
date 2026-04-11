ce58daf078710f31556830ad3cba4f58bcaa9cae
MaddySingh2003 <Milansuryavanshi27@gmail.com>
2026-04-11 13:30:26 +0530

    set

---
+++ Cogniview AI/ml-service/prepare_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df=pd.read_csv("train.csv")
+
+
+df=df.rename(columns={
+     "questions": "question",
+    "student_answer": "answer",
+    "teacher_marks": "score"
+})
+
+def convert_label(score):
+    if score>=4:
+        return "good"
+    elif score>=2:
+        return "avarage"
+    else:
+        return "poor"
+    
+df["label"]=df["score"].apply(convert_label)
+# keep only required columns
+df = df[["question", "answer", "label"]]
+
+df=df.dropna()
+
+df.to_csv("clean_train.csv",index=False)
+print("clean dataset saved")
